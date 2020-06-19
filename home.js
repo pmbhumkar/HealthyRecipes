553b0cee14933c3e1132f2f05faaf53f3babcb1d
Bhumkar <Pravin.Bhumkar@emc.com>
2020-06-19 11:59:28 +0530

    Added Materialize CSS components

--- home.js
+++ home.js
@@ -1,67 +1,125 @@
-var btn = document.getElementById("submit");
-btn.addEventListener("click", function() {
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function () {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+    // alert('Hey there!');
+
+});
+
+
+var btn = document.getElementById("search-btn");
+
+function createList(recipeList, recipe) {
+    let li = document.createElement('li');
+    let title = document.createElement('div');
+    let body = document.createElement('div');
+    let img = document.createElement('img');
+    let span = document.createElement('span')
+
+    title.setAttribute('class', 'collapsible-header blue darken-1 white-text waves-effect waves-light');
+    body.setAttribute('class', 'collapsible-body blue lighten-4');
+
+    img.setAttribute('src', recipe.strMealThumb);
+    img.setAttribute('class', 'align-left');
+    img.setAttribute('height', '50px');
+    img.setAttribute('width', '50px');
+    span.setAttribute('id', 'recipe_title');
+
+    span.textContent = recipe.strMeal;
+    // body.textContent = 'Sample Recipe';
+
+    title.appendChild(img);
+    title.appendChild(span);
+    li.appendChild(title);
+    li.appendChild(body);
+    recipeList.appendChild(li);
+
+    let loaded = false;
+
+
+    title.addEventListener('click', function () {
+        if (!loaded) {
+            showRecipe(body, recipe.idMeal);
+            loaded = true;
+        }
+    })
+}
+
+function noData(ingredient) {
+    var elems = document.querySelector('.modal');
+    let error = document.querySelector('#error_msg');
+    error.textContent = `Recipe with ingredient ${ingredient} not found`;
+    var instance = M.Modal.getInstance(elems);
+    instance.open();
+}
+
+
+btn.addEventListener("click", function () {
     var request = new XMLHttpRequest();
     let ingredient = document.getElementById("ing").value;
-    request.open('GET', 'https://www.themealdb.com/api/json/v1/1/filter.php?i='+ingredient, true);
+    request.open('GET', 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + ingredient, true);
 
-    request.onload = function() {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response).meals;
-    var table = document.getElementById("recipeList");
-    let i=0;
-    table.innerHTML = "";
-    data.forEach(recipe => {
-      // insert rows in table
-            let row = table.insertRow(i);
-            let recipeImg = row.insertCell(0);
-            let recipeName = row.insertCell(1);
-            recipeImg.style = "width: 120px; border-top:1px solid black; border-bottom:1px solid black";
-            recipeName.innerHTML = recipe.strMeal;
-            recipeName.classList += "recipetitle";
-            recipeImg.innerHTML = `<img src='${recipe.strMealThumb}' height='100px' width='100px'>`;
-            recipeName.addEventListener('click', function()
-            {
-                showRecipe(recipe.idMeal);
-                this.classList.add("selectedRow");
-            })
-            i++;
-        });
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response).meals;
+        let recipeList = document.querySelector("#recipe-list");
+        while (recipeList.lastElementChild) {
+            recipeList.removeChild(recipeList.lastElementChild);
+        }
+
+        if (data != null) {
+            // recipeList.removeChild();
+            data.forEach(recipe => {
+                createList(recipeList, recipe);
+            });
+        } else {
+            noData(ingredient);
+            // console.log("No data found");
+        }
+
+
     }
 
     // Send request
     request.send();
 });
 
-function showRecipe(recipeId)
-{
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i='+recipeId, true);
-       let prevSelected = document.getElementsByClassName("selectedRow");
-       if (prevSelected.length > 0)
-       {
-        prevSelected[0].classList.remove("selectedRow");
-       }
+function showRecipe(body, recipeId) {
 
-    request.onload = function() {
-    // Begin accessing JSON data here
-    var recipe = JSON.parse(this.response).meals[0];
-    let recipeDetails = document.getElementById('recipeDetails');
-    recipeDetails.innerHTML = `<b>Cuisin :</b> ${recipe.strArea} <br>`;
-    recipeDetails.innerHTML += `<b>Ingredients : </b><br>`
-    for(let i=1; i<= 20; i++)
-    {
-        let ingredient = eval("recipe.strIngredient"+i);
-        if(ingredient === "")
-        {
-            break;
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + recipeId, true);
+
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var recipe = JSON.parse(this.response).meals[0];
+
+
+        // console.log(recipe);
+        body.innerHTML = "<b>Ingredients:</b> <br>"
+        let ul = document.createElement('ul');
+        for (let i = 1; i <= 20; i++) {
+            let li = document.createElement('li');
+            
+            let ingredient = recipe[`strIngredient${i}`];
+            if (ingredient === "") {
+                break;
+            }
+            li.textContent = ingredient + ' - ' + recipe[`strMeasure${i}`];
+            // body.innerHTML += ingredient + ' - ' +  + "<br>";
+            ul.appendChild(li);
         }
-        recipeDetails.innerHTML += ingredient + ' - ' + eval("recipe.strMeasure"+i) + "<br>";
-    }
-        recipeDetails.innerHTML += '<b> Instructions : </b><br>';
-        recipeDetails.innerHTML += recipe.strInstructions;
-        recipeDetails.innerHTML += `<br> <iframe width="560" height="315" src="${recipe.strYoutube.replace('watch?v=','embed/')}" frameborder="0" 
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        body.appendChild(ul);
+        body.innerHTML += "<hr><b>Instructions:</b> <br>";
+        body.innerHTML += recipe.strInstructions;
+        body.innerHTML += "<br><hr>"
+        body.innerHTML += "<i>Follow recipe on YouTube</i><br>";
+        body.innerHTML += `<iframe width="560" height="315" src="${recipe.strYoutube.replace('watch?v=', 'embed/')}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
     }
 
     request.send();
 }
+
